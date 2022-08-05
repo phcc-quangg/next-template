ec54ce648750bddddbd6451c21c594b723dbcb2e
phc-quangg <quang.tang@thuocsi.vn>
2022-08-05 13:16:17 +0700

    feat: init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  env: {
+    API_HOST: process.env.API_HOST,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        
+      },
+      colors: {
+        
+      },
+      fontSize: {
+        12: '.75rem',
+        14: '.875rem',
+        16: '1rem',
+        18: '1.125rem',
+      },
+      backgroundPosition: {
+        
+      },
+
+      width: {
+      },
+
+      height: {
+      },
+
+      margin: {
+      },
+      top: {
+      },
+      left: {
+      },
+
+      animation: {
+      },
+
+      keyframes: {
+      },
+    },
+  },
+  plugins: [],
+};
